c75f8881f6826de3cf4fa94b3ec6ed3ccce69235
Julien Thiphineau <j.thiphineau@yahoo.fr>
2023-04-12 15:23:40 +0200

    requete

--- app/src/main/java/com/doranco/mysqlite/MainActivity.java
+++ app/src/main/java/com/doranco/mysqlite/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,6 +46,18 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    private void get(View arg){
+        Cursor cursor=sd.rawQuery("select *from user where id=:ida", new String[]{"1"});
+        cursor.moveToFirst();
+        int idm=cursor.getInt(cursor.getColumnIndexOrThrow("ida"));
+        String nom=cursor.getString(cursor.getColumnIndexOrThrow("nom"));
+        int age=cursor.getInt(cursor.getColumnIndexOrThrow("age"));
+        String fonction=cursor.getString(cursor.getColumnIndexOrThrow("fonction"));
+        nom_txt.setText(nom);
+        age_txt.setText(age+"");
+        fonction_txt.setText(fonction);
+        cursor.close();
     }
 
 }
